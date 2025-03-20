ccd05dde4e2b8a57333e81a79751b3d92f0d5514
stefanolazzari <stefanolazzari79@libero.it>
2025-03-20 08:47:58 +0000

    array.js

---
+++ array.js
@@ -0,0 +1,19 @@
+let colori = ["red","black","white","green","yellow","pink"];
+
+let contenitore = document.getElementById("contenitore");
+
+
+for (let index = 0; index < colori.length; index++) {
+    const elemento = colori[index];
+    const coloreTesto = colori[colori.length-1 - index]
+    console.log(elemento);
+
+    const nuovoParagrafo = document.createElement("p");
+    nuovoParagrafo.textContent = elemento;
+    nuovoParagrafo.style.backgroundColor = elemento;
+    nuovoParagrafo.style.color = coloreTesto;
+    
+
+
+    contenitore.appendChild(nuovoParagrafo);
+}
---
+++ main.js
@@ -0,0 +1,60 @@
+
+
+
+
+for(let y = 0; y < 100; y++){
+    console.log("Ora il valore di y é: " + y);
+}
+
+
+
+
+
+
+
+
+//ciclo for 
+for(let contatore = 0; contatore < 5;contatore++){
+    console.log("Ha detto ciao per " + contatore + " volte!");
+}
+
+
+
+
+for (let index = 0; index < 20; index++) {
+    let numeroCasualeDec = Math.random() * 100;
+    let numeroCasualeArr = Math.floor(numeroCasualeDec);
+    console.log(numeroCasualeArr);
+    
+}
+
+
+
+
+let contenitore2 = document.getElementById("contenitore");
+
+
+for (let index = 0; index < 15; index++) {
+    const nuovoElemento = document.createElement("button");
+    nuovoElemento.textContent = index;
+    nuovoElemento.style.textAlign = "center";
+    nuovoElemento.style.backgroundColor = "red";
+    nuovoElemento.style.padding = "1%";
+    nuovoElemento.style.margin = "1%";
+    contenitore2.appendChild(nuovoElemento);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
