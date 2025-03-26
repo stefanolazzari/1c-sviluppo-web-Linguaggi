b6902bef5d90a70da7436f5af73c8ed9afe5df4a
stefanolazzari <stefanolazzari79@libero.it>
2025-03-26 11:36:32 +0000

    immagini

--- array.js
+++ array.js
@@ -1,5 +1,7 @@
 let colori = ["red","black","white","green","yellow","pink"];
 
+let arrayImmagini = ["cammello.jpeg","giraffa.jpeg","gufo.jpeg"];
+
 let contenitore = document.getElementById("contenitore");
 
 
@@ -12,11 +14,23 @@ for (let index = 0; index < colori.length; index++) {
     nuovoParagrafo.textContent = elemento;
     nuovoParagrafo.style.backgroundColor = elemento;
     nuovoParagrafo.style.color = coloreTesto;
+    nuovoParagrafo.style.textAlign = "center";
     nuovoParagrafo.style.marginRight = index * 5 + "%";
     nuovoParagrafo.style.marginLeft = index * 5 + "%";
-
     nuovoParagrafo.style.fontSize = (index+1)*10  + "px";
+    nuovoParagrafo.style.padding = index + "%";
 
 
     contenitore.appendChild(nuovoParagrafo);
 }
+
+
+
+for (let index = 0; index < arrayImmagini.length; index++) {
+    const nuovaImmagine = document.createElement("img");
+    nuovaImmagine.src = arrayImmagini[index];
+    nuovaImmagine.alt = arrayImmagini[index];
+    contenitore.appendChild(nuovaImmagine);
+}
+
+
