f46dc9ed9f4296f0f79ada0bb63544826336dc4c
stefanolazzari <stefanolazzari79@libero.it>
2025-03-27 08:37:31 +0000

    aggiunte funzioni modifica css

---
+++ funzioni.js
@@ -0,0 +1,16 @@
+
+
+function cambiaSfondoInVerde(){
+    const elementoContenitore = document.getElementById("contenitore");
+    elementoContenitore.style.backgroundColor = "green";
+}
+
+function cambiaMargine(){
+    const elementoContenitore = document.getElementById("contenitore");
+    elementoContenitore.style.margin = "5%";
+
+}
+
+
+
+
