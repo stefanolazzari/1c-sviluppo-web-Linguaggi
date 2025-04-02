f690e7d43477d92b2ee3d5980d906e2ef71f638a
stefanolazzari <stefanolazzari79@libero.it>
2025-04-02 06:59:23 +0000

    esempio tombola

--- esempiCicloFor.js
+++ esempiCicloFor.js
@@ -19,12 +19,28 @@ for (let index = 0; index < 20; index++) {
 let contenitore2 = document.getElementById("contenitore");
 
 
-for (let index = 0; index < 15; index++) {
-    const nuovoElemento = document.createElement("button");
-    nuovoElemento.textContent = index;
-    nuovoElemento.style.textAlign = "center";
-    nuovoElemento.style.backgroundColor = "red";
-    nuovoElemento.style.padding = "1%";
-    nuovoElemento.style.margin = "1%";
-    contenitore2.appendChild(nuovoElemento);
+// for (let index = 0; index < 15; index++) {
+//     const nuovoElemento = document.createElement("button");
+//     nuovoElemento.textContent = index;
+//     nuovoElemento.style.textAlign = "center";
+//     nuovoElemento.style.backgroundColor = "red";
+//     nuovoElemento.style.padding = "1%";
+//     nuovoElemento.style.margin = "1%";
+//     contenitore2.appendChild(nuovoElemento);
+// }
+
+
+
+
+
+let arrayValori = [34,76,90,2,78,89];
+let somma = 0;
+
+for(let i = 0; i <  arrayValori.length; i++ ){
+    if(arrayValori[i] == 90){
+        console.log("la paura");
+    }
+    
+    somma = somma + arrayValori[i];
+    console.log(somma);
 }
